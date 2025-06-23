5cfc8e3c00e4ea4213a86a93a7b41c386842b245
Abdalla2004-collab <abdallabakr8072@gmail.com>
2025-06-23 11:22:47 +0100

    styling the website

--- app.py
+++ app.py
@@ -1,19 +1,74 @@
 # Imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, request
 from flask_scss import Scss
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 # My App
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+db = SQLAlchemy(app)
+
+#data class 
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"Task {self.id}" 
 
 
 
-@app.route("/")
+#routes to webpage
+@app.route("/", methods=["POST", "GET"])
 def index():
-    return render_template("index.html")
+    # Add a task
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"ERROR: {e}")
+            return(f"ERROR: {e}")
+    # See all current tasks
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template("index.html", tasks=tasks)
 
+#for deleting items new route will be created for redirecting
+@app.route("/delete/<int:id>")
+def delete(id:int):
+    delete_task = MyTask.query.get_or_404(id)
+    try:
+        db.session.delete(delete_task)
+        db.session.commit()
+        return redirect("/")
+    except Exception as e:
+        print (f"ERROR: {e}")
+        return f"ERROR: {e}"
 
+#for edditing existing tasks
+@app.route("/edit/<int:id>", methods=["GET", "POST"])
+def edit(id:int):
+    task = MyTask.query.get_or_404(id)
+    if request.method == "POST":
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            return f"ERROR: {e}"
+    else:
+        return render_template("edit.html", task=task)
 
+#runner and debugger
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
 
-if __name__ in "__main__":
     app.run(debug=True)
